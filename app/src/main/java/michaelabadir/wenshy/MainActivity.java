c2b6e54df857a74dbd1528ec4d38211dbb5b99ce
MichaelAbadir <michael.abadir@hotmail.com>
2018-01-17 13:33:49 +0200

    Call Driver
    Button to call driver

--- app/src/main/java/michaelabadir/wenshy/MainActivity.java
+++ app/src/main/java/michaelabadir/wenshy/MainActivity.java
@@ -1,7 +1,10 @@
 package michaelabadir.wenshy;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_call).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialContactPhone("123123123");
+            }
+        });
     }
+
+    private void dialContactPhone(final String phoneNumber) {
+        startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
+    }
+
 }
