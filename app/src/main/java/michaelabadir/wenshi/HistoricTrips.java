db45fd3d99fe3e9f3756d0fe43b54a37a606bee7
MichaelAbadir <michael.abadir@hotmail.com>
2018-02-14 13:39:38 +0200

    Navigation Drawer
    Added Navigation Drawer Menu plus enabling Historic Trips & Payments in the Navigation Drawer (Changing them to fragments).

--- app/src/main/java/michaelabadir/wenshi/HistoricTrips.java
+++ app/src/main/java/michaelabadir/wenshi/HistoricTrips.java
@@ -1,12 +1,14 @@
 package michaelabadir.wenshi;
 
-import android.app.Activity;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,21 +19,49 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class HistoricTrips extends Activity {
+public class HistoricTrips extends Fragment {
 
     DatabaseReference rootRef;
     TextView demoValue;
     static String date, from, to, cost;
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_historic_trips, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        ArrayList<Trip> Trip = GetTrip();
+        demoValue = getView().findViewById(R.id.tvValue);
+        final ListView lv1 = getView().findViewById(R.id.listView_historic_trips);
+
+        lv1.setAdapter(new MyCustomBaseAdapter(getActivity(), Trip));
+
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                //Object o = lv1.getItemAtPosition(position);
+                //Trip fullObject = (Trip) o;
+                //Toast.makeText(ListViewBlogPost.this, "You have chosen: " + " " + fullObject.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+
+/*
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_historic_trips);
+   ////     setContentView(R.layout.activity_historic_trips);
 
         ArrayList<Trip> Trip = GetTrip();
-        demoValue = findViewById(R.id.tvValue);
+   ////     demoValue = findViewById(R.id.tvValue);
 
-        final ListView lv1 = findViewById(R.id.listView_historic_trips);
+   ////     final ListView lv1 = findViewById(R.id.listView_historic_trips);
         lv1.setAdapter(new MyCustomBaseAdapter(this, Trip));
 
         lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -43,8 +73,8 @@ public class HistoricTrips extends Activity {
             }
         });
 
-
     }
+*/
 
     private ArrayList<Trip> GetTrip(){
         final ArrayList<Trip> results = new ArrayList<>();
@@ -56,6 +86,7 @@ public class HistoricTrips extends Activity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for(DataSnapshot child : dataSnapshot.getChildren()) {
                     HashMap<String, String> value = (HashMap<String, String>) child.getValue();
+                    assert value != null;
                     date = value.get("date");
                     from = value.get("from");
                     to = value.get("to");
@@ -69,7 +100,7 @@ public class HistoricTrips extends Activity {
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                Toast.makeText(HistoricTrips.this, "Error", Toast.LENGTH_SHORT).show();
+             //   Toast.makeText(HistoricTrips.this, "Error", Toast.LENGTH_SHORT).show();
             }
         });
         return results;
--- app/src/main/java/michaelabadir/wenshi/PaymentOptions.java
+++ app/src/main/java/michaelabadir/wenshi/PaymentOptions.java
@@ -2,19 +2,41 @@ package michaelabadir.wenshi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class PaymentOptions extends AppCompatActivity {
+public class PaymentOptions extends Fragment {
 
     private RadioButton rb1;
     private TextView resultText;
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_payment_options, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        RadioGroup radioGroup = getView().findViewById(R.id.radiogroup);
+        radioGroup.check(R.id.radioButton_payment_cash);
+
+        addListenerOnButton();
+
+
+    }
+
+/*
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +47,13 @@ public class PaymentOptions extends AppCompatActivity {
         addListenerOnButton();
 
     }
-
+*/
     public void addListenerOnButton() {
 
 
-        Button btnDisplay = findViewById(R.id.button_payment_submit);
+        Button btnDisplay = getView().findViewById(R.id.button_payment_submit);
 
-        rb1 = findViewById(R.id.radioButton_payment_cash);
+        rb1 = getView().findViewById(R.id.radioButton_payment_cash);
 
         btnDisplay.setOnClickListener(new OnClickListener() {
 
@@ -41,13 +63,12 @@ public class PaymentOptions extends AppCompatActivity {
              //   radioButton = (RadioButton) findViewById(selectedId);
              //   Toast.makeText(PaymentOptions.this, "Hello From the other sideeee!!!", Toast.LENGTH_SHORT).show();
                 if(rb1.isChecked()) {
-                    resultText = findViewById(R.id.textView_payment_result);
+                    resultText = getView().findViewById(R.id.textView_payment_result);
                     resultText.setText(rb1.getText());
                 }
                 else    {
-                    Intent ccEntry = new Intent(getApplicationContext(), CreditCardEntry.class);
+                    Intent ccEntry = new Intent(getActivity().getApplicationContext(), CreditCardEntry.class);
                     startActivityForResult(ccEntry, 0);
-
                 }
             }
 
