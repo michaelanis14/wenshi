ccf210e28ebedd75ff7ab126226a7d1fb87192cb
MichaelAbadir <michael.abadir@hotmail.com>
2018-02-22 15:54:16 +0200

    Integrating Four Navigation Items
    1. Historic Trips (Complete).
    2. Payment (Without real integration).
    3. Help (Demo).
    4. Rate and Charge (Demo).

---
+++ app/src/main/java/com/wenshi_egypt/wenshi/PaymentOptions.java
@@ -0,0 +1,71 @@
+package com.wenshi_egypt.wenshi;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class PaymentOptions extends Fragment implements OnClickListener{
+
+    private RadioButton rb1;
+    private TextView resultText;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_payment_options, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        //noinspection ConstantConditions
+        RadioGroup radioGroup = getView().findViewById(R.id.radiogroup);
+        radioGroup.check(R.id.radioButton_payment_cash);
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        //noinspection ConstantConditions
+        Button btnDisplay = getView().findViewById(R.id.button_payment_submit);
+        rb1 = getView().findViewById(R.id.radioButton_payment_cash);
+
+        btnDisplay.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(rb1.isChecked()) {
+                    resultText = getView().findViewById(R.id.textView_payment_result);
+                    resultText.setText(rb1.getText());
+                }
+                else    {
+                    Intent ccEntry = new Intent(getActivity().getApplicationContext(), CreditCardEntry.class);
+                    startActivityForResult(ccEntry, 0);
+                }
+            }
+
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/com/wenshi_egypt/wenshi/Trip.java
@@ -0,0 +1,51 @@
+package com.wenshi_egypt.wenshi;
+
+import java.text.DecimalFormat;
+
+public class Trip {
+    private String date, from, to;
+    private double cost;
+
+    Trip(String date, String from, String to, double cost)   {
+        this.date = date;
+        this.from = from;
+        this.to = to;
+        this.cost = cost;
+    }
+
+    String getDate() {
+        return date;
+    }
+
+    @SuppressWarnings("unused")
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    String getTo() {
+        return to;
+    }
+
+    @SuppressWarnings("unused")
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    String getCost() {
+        DecimalFormat format = new DecimalFormat("0.#");
+        return format.format(cost);
+    }
+
+    @SuppressWarnings("unused")
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
