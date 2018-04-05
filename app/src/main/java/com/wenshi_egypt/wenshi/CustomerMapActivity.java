d11a097e75ce11b48d94e33d26d0893a2b25b413
Michael Anis <michael@merply.com>
2018-04-05 15:05:22 +0200

    Removed unnessecary menu

--- app/src/main/java/com/wenshi_egypt/wenshi/CustomerMapActivity.java
+++ app/src/main/java/com/wenshi_egypt/wenshi/CustomerMapActivity.java
@@ -1021,14 +1021,7 @@ public class CustomerMapActivity extends AppCompatActivity implements GetDirecti
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
 
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
